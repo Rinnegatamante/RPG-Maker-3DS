0731cd3db11f63c88a9242dc30f01b57f69a6875
Rinnegatamante <rinnegatamante@gmail.com>
2015-08-15 14:39:29 +0200

    Re-organized maps folder.
    Re-organized maps folder.

---
+++ maps/map1/map.lua
@@ -0,0 +1,28 @@
+-- Random Encounter Setting
+rnd_encounter = true
+
+-- Map Size
+map_width = 960
+map_height = 256
+
+-- Map table with blocks description
+map_table = {
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+	2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+	2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2,
+	1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+	}
+
+-- Loading pre-rendered map	
+map_i = Screen.loadImage(System.currentDirectory().."/maps/"..map.."/level1.png")
+map2_i = Screen.loadImage(System.currentDirectory().."/maps/"..map.."/level2.png")
+map3_i = Screen.loadImage(System.currentDirectory().."/maps/"..map.."/level3.png")
+
+-- Map conversion to GPU texture
+map_l1 = Graphics.loadImage(map_i)
+map_l2 = Graphics.loadImage(map2_i)
+map_l3 = Graphics.loadImage(map3_i)
