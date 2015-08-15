4dc841f6d3e28c238652537a1afd1f50efc6bd68
Rinnegatamante <rinnegatamante@gmail.com>
2015-08-15 14:38:50 +0200

    Added Map Events Sample.
    Added Map Events Sample.

---
+++ maps/map1/events.lua
@@ -0,0 +1,16 @@
+
+function MapEvents()
+
+	-- EVENT 1 - Walking trigger type Event (Oneshot)
+	if not event1 then
+		if pos_x == 21 and pos_y == 3 then
+			ShowDialog("I heard a strange noise. I hope there is nothing to worry about. I should check where it came from, maybe it was made by rats...")
+			event1 = true
+		end
+		if pos_x == 22 and pos_y == 3 then
+			ShowDialog("I heard a strange noise. I hope there is nothing to worry about. I should check where it came from, maybe it was made by rats...")
+			event1 = true
+		end
+	end
+	
+end
