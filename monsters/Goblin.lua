920579550ea4c388787dc3f4c7f2f39a5c3e1dc3
Rinnegatamante <rinnegatamante@gmail.com>
2015-08-16 17:19:37 +0200

    Added Goblin to monster test database.
    Added Goblin to monster test database.

---
+++ monsters/Goblin.lua
@@ -0,0 +1,29 @@
+-- Basic Information
+name = "Goblin"
+sprite = "Goblin"
+
+-- Attack Settings
+attack_type = "Axe"
+attack_min = 1
+attack_max = 5
+
+-- Magic Settings
+spells = {}
+
+-- Statistics
+defense = 0
+level = 1
+hp = 80
+mp = 0
+
+-- Rewards
+experience = 2
+loot = {}
+
+-- Weakness/Resistence/Immunity
+immunity = {}
+weakness = {}
+resistence = {}
+
+-- Array Info
+monster_info = {name, attack_type, attack_min, attack_max, spells, defense, level, hp, mp, experience, loot, immunity, weakness, resistence}
