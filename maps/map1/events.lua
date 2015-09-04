7da5d0113a5a85e1c84d1bd4abce3acb0243867c
Rinnegatamante <rinnegatamante@gmail.com>
2015-09-04 13:54:44 +0200

    Solved a little bug with events triggering.
    Solved a little bug with events triggering.

--- maps/map1/events.lua
+++ maps/map1/events.lua
@@ -6,11 +6,14 @@ function MapEvents()
 		if pos_x == 21 and pos_y == 3 then
 			ShowDialog("I heard a strange noise. I hope there is nothing to worry about. I should check where it came from, maybe it was made by rats...")
 			event1 = true
+			return true
 		end
 		if pos_x == 22 and pos_y == 3 then
 			ShowDialog("I heard a strange noise. I hope there is nothing to worry about. I should check where it came from, maybe it was made by rats...")
 			event1 = true
+			return true
 		end
 	end
 	
+	return false
 end
