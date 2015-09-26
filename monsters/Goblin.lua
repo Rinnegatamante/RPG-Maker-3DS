2e684f85aea484646f481638339136c9bd0c4b91
Rinnegatamante <rinnegatamante@gmail.com>
2015-09-26 12:08:24 +0200

    Added missing information.
    Added missing information.

--- monsters/Goblin.lua
+++ monsters/Goblin.lua
@@ -3,27 +3,40 @@ name = "Goblin"
 sprite = "Goblin"
 
 -- Attack Settings
-attack_type = "Melee"
+attack_type = "melee"
 attack_min = 1
 attack_max = 5
+magic_attack = 0
+crit_hit = 5
 
 -- Magic Settings
 spells = {}
 
 -- Statistics
 defense = 0
+magic_defense = 0
 level = 1
 hp = 80
 mp = 0
+speed = 50
 
 -- Rewards
 experience = 2
 loot = {}
 
 -- Weakness/Resistence/Immunity
-immunity = {}
-weakness = {}
-resistence = {}
+resistence = {
+		["melee"] = 0,
+		["ranged"] = 0,
+		["magic"] = 0,
+		["fire"] = 0,
+		["ice"] = 0,
+		["thunder"] = 0,
+		["water"] = 0,
+		["sancta"] = 0,
+		["dark"] = 0,
+		["ade"] = 0
+	}
 
 -- Custom I.A.
 custom_ia = false
